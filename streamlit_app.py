762600a6c84f9731e48cbc8b56f384a20d486863
queenshaelya <queenshaelyaaziza@gmail.com>
2025-05-27 11:01:16 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,5 @@
 import streamlit as st
 
 st.title("Selamat Datang di Web Queen")
-st.write(
-    "ngoding seru bareng Queen 😆👐🏻"
-)
+st.write("ngoding seru bareng Queen 😆👐🏻")
 st.image("IMG_7798.jpeg", width=200)
